1f77f3365aa292fbb138ef109146f001d97f06b5
Vladyslav Zhadan <zhadavla@fit.cvut.cz>
2023-05-26 20:17:50 +0200

    adding shadows

--- thedrake/ui/DrakeUI.java
+++ thedrake/ui/DrakeUI.java
@@ -3,7 +3,9 @@ package thedrake.ui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 import java.util.Objects;
@@ -11,6 +13,7 @@ import java.util.Objects;
 public class DrakeUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(DrakeUI.class.getResource("src/mainMenu.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
